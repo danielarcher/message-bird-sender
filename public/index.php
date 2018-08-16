c6588a0fcf3d0a6a6a6e2b3ce3e3f83cadbe9bd1
Daniel Archer <contato@danielcramer.com.br>
2018-08-16 03:38:17 -0300

    using the new control classes to send a example message

--- public/index.php
+++ public/index.php
@@ -3,15 +3,44 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Sender;
+use App\Storage;
+use App\Udh;
 
 $config = require __DIR__ . '/../config/' . getenv('APPLICATION_ENV') . '.php';
 
-$messageBirdClient = new \MessageBird\Client($config['api-key']); // Set your own API access key here.
+function split_message($messageBody) {
+    
+    $splitedMessage = str_split($messageBody,153);
+    $total = count($splitedMessage);
+    
+    foreach($splitedMessage as $key => $part)
+    {
+        yield [
+            'udh' => (new Udh($total, ++$key))->toString(), 
+            'body' => $part
+        ];
+    }
+}
 
+$messageText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+$storage = new Storage();
+foreach(split_message($messageText) as $messagePart) {
+    $storage->add([
+        'originator' => $config['originator'],
+        'recipients' => [31612345678], 
+        'body'       => $messagePart['body'],
+        'udh'        => $messagePart['udh']
+    ]);
+}
+
+$messageBirdClient = new \MessageBird\Client($config['api-key']);
+
+$returnedData = $storage->get();
 $message             = new \MessageBird\Objects\Message();
-$message->originator = 'MessageBird';
-$message->recipients = array(31612345678);
-$message->body       = 'This is a test message.';
+$message->originator = $returnedData['originator'];
+$message->recipients = $returnedData['recipients'];
+$message->setBinarySms($returnedData['udh'], $returnedData['body']);
 
 try {
     $sender = new Sender($messageBirdClient);
