1ee60d4b1f25081a51942b20827665cabee145ce
Daniel Archer <contato@danielcramer.com.br>
2018-08-16 20:38:21 -0300

    add libsodium key in config

--- config/development.php
+++ config/development.php
@@ -7,4 +7,6 @@ return [
     'data-file'        => __DIR__ . '/../data/messages_to_send.txt',
     'sleep-sec'        => 1,
     'queue-id'         => 8756342,
+    'sodium-key'       => 'a4018eb2093323aacd6055e84a9f4f16e2fb0041e91a1fed075c59441ac92196',
+    'sodium-nonce'     => 'b8234623cb15f9c68d47c6d36d191de39e8ab1f9d2238926'
 ];
