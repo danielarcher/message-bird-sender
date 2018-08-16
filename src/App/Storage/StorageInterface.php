cacd29222692b8a0765a7ddd54225da042e0ed36
Daniel Archer <contato@danielcramer.com.br>
2018-08-16 20:36:38 -0300

    altered storage interface

--- src/App/Storage/StorageInterface.php
+++ src/App/Storage/StorageInterface.php
@@ -2,11 +2,14 @@
 
 namespace App\Storage;
 
+use App\Decorator\DecoratorInterface;
+
 /**
  * Storage class, responsible for save the received data and return then when asked
  */
 interface StorageInterface
 {
+    public function setDecorator(DecoratorInterface $decorator);
     /**
      * Add new itens on the storage
      * @param array $data 
