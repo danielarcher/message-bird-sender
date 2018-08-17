70f6bcb2e5bfba9ec678564c5a1a35fb47334ffb
Daniel Archer <contato@danielcramer.com.br>
2018-08-17 12:39:11 -0300

    improved docs and change receiver construction

--- public/index.php
+++ public/index.php
@@ -2,15 +2,34 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Controller\ReceiverController;
+use App\Receiver;
 
 $env = getenv('APPLICATION_ENV') ?? 'development';
 $config = require __DIR__ . '/../config/' . $env . '.php';
 
+$response = 'Welcome Message Bird!';
+
 if ($_POST) {
+
+    /**
+     * Validate recipients as array
+     */
     $recipients = is_array($_POST['recipients']) ? $_POST['recipients'] : [$_POST['recipients']];
 
-    $controller = new ReceiverController($config);
+    /**
+     * Storage mode
+     */
+    $storage = new QueueStorage($this->config['queue-id']);
 
-    echo $controller->post($recipients, $_POST['body']);
+    /**
+     * Decorator
+     */
+    $storate->setDecorator(new LibSodiumDecorator($this->config['sodium-key'], $this->config['sodium-nonce']));
+
+    $controller = new Receiver($config, $storage, $decorator);
+    $response = $controller->post($recipients, $_POST['body']);
+
 }
+
+
+return $response;
