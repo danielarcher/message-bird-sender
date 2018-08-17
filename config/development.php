f6c0b2bcc2e156e04893fc426e4600388f94119c
Daniel Archer <contato@danielcramer.com.br>
2018-08-17 11:38:56 -0300

    altered max messages per second

--- config/development.php
+++ config/development.php
@@ -1,13 +1,13 @@
 <?php 
 
 return [
-	'api-key'          => 'oCUOTVIzebR0oqnFpE88pAsEy',
-    'originator'       => 'MessageBird',
-    'message-max-size' => 160,
-    'data-file'        => __DIR__ . '/../data/messages_to_send.txt',
-    'log-file'         => __DIR__ . '/../logs/app.log',
-    'sleep-sec'        => 1,
-    'queue-id'         => 8756342,
-    'sodium-key'       => 'a4018eb2093323aacd6055e84a9f4f16e2fb0041e91a1fed075c59441ac92196',
-    'sodium-nonce'     => 'b8234623cb15f9c68d47c6d36d191de39e8ab1f9d2238926'
+	'api-key'                   => 'oCUOTVIzebR0oqnFpE88pAsEy',
+    'originator'                => 'MessageBird',
+    'message-max-size'          => 160,
+    'data-file'                 => __DIR__ . '/../data/messages_to_send.txt',
+    'log-file'                  => __DIR__ . '/../logs/app.log',
+    'limit-messages-per-second' => 1,
+    'queue-id'                  => 8756342,
+    'sodium-key'                => 'a4018eb2093323aacd6055e84a9f4f16e2fb0041e91a1fed075c59441ac92196',
+    'sodium-nonce'              => 'b8234623cb15f9c68d47c6d36d191de39e8ab1f9d2238926'
 ];
